b05838e5c911815f32ab39b86eb2b0b5a279fe89
rezashnia1234 <reza_shnia@spam4.me>
2015-01-01 20:26:40 +0330

    38

--- www/js/page_content.js
+++ www/js/page_content.js
@@ -7,7 +7,7 @@ function getParameterByName(name) {
 		results = regex.exec(location.search);
 	return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
-function getURL(URL,cache) {
+function getURL(URL,cache,iframe) {
 	var networkState = navigator.connection.type;
 	if (networkState == Connection.NONE) {
 		var article_json = window.localStorage.getItem(md5(URL));
@@ -21,17 +21,24 @@ function getURL(URL,cache) {
 			$('.container').html("»—«Ì „‘«ÂœÂ «Ì‰ ’›ÕÂ ‰Ì«“ »Â «Ì‰ —‰  œ«—Ìœ");
 		}
 	} else {
-		console.log('SMGROUP ::::::::::::::::::::::::::::::::::::    get_URL / Start Download JSON For cache');
-		$.ajax({ type: "GET",   
-				url: URL,
-				async: false,
-				success : function(text)
-				{
-					$('.container').html(text);
-					if(cache == true)
-						window.localStorage.setItem(md5(URL),text);
-				}
-		});
+		if(iframe == false)
+		{
+			console.log('SMGROUP ::::::::::::::::::::::::::::::::::::    get_URL / Start Download JSON For cache');
+			$.ajax({ type: "GET",   
+					url: URL,
+					async: false,
+					success : function(text)
+					{
+						$('.container').html(text);
+						if(cache == true)
+							window.localStorage.setItem(md5(URL),text);
+					}
+			});
+		}
+		else
+		{
+			$('.container').html("<iframe src='" + URL + "'></iframe>");
+		}
 	}
 
 
