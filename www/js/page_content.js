1eab42915f4084bcdaf4578e877c8c470c8521b1
rezashnia1234 <reza_shnia@spam4.me>
2015-01-02 04:06:07 +0330

    42

--- www/js/page_content.js
+++ www/js/page_content.js
@@ -1,6 +1,13 @@
 var pathname = window.location.pathname; // Returns path only
 var url      = window.location.href;     // Returns full URL
 
+$(function () {
+    $('.container a').on("click", function (e) {
+        e.preventDefault();
+		alert("00");
+    });
+});
+
 function getParameterByName(name) {
 	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
 	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
@@ -8,6 +15,17 @@ function getParameterByName(name) {
 	return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 function getURL(URL,cache,iframe) {
+	cache = cache || true;
+	iframe = iframe || false;
+	if(iframe == "false")
+		iframe = false;
+	if(iframe == "true")
+		iframe = true;
+	if(cache == "false")
+		cache = false;
+	if(cache == "true")
+		cache = true;
+		
 	var networkState = navigator.connection.type;
 	if (networkState == Connection.NONE) {
 		var article_json = window.localStorage.getItem($.md5(URL));
@@ -18,7 +36,7 @@ function getURL(URL,cache,iframe) {
 		else
 		{
 			console.log('SMGROUP ::::::::::::::::::::::::::::::::::::    get_URL / no internet AND no cache');
-			$('.container').html("ÈÑÇí ãÔÇåÏå Çíä ÕÝÍå äíÇÒ Èå ÇíäÊÑäÊ ÏÇÑíÏ");
+			$('.container').html("Ø¨Ø±Ø§ÙŠ Ù…Ø´Ø§Ù‡Ø¯Ù‡ Ø§ÙŠÙ† ØµÙØ­Ù‡ Ù†ÙŠØ§Ø² Ø¨Ù‡ Ø§ÙŠÙ†ØªØ±Ù†Øª Ø¯Ø§Ø±ÙŠØ¯");
 		}
 	} else {
 		if(iframe == false)
@@ -30,6 +48,22 @@ function getURL(URL,cache,iframe) {
 					success : function(text)
 					{
 						$('.container').html(text);
+						
+						$('.container a').on("click", function (e) {
+							e.preventDefault();
+							if($(this).attr("href").toLowerCase().indexOf("http://smgroup.ir/parse3/") >= 0)
+								getURL($(this).attr("href"),$(this).attr("cache"),$(this).attr("iframe"));
+							else if($(this).attr("href").toLowerCase().indexOf("http://www.smgroup.ir/parse3/") >= 0)
+								getURL($(this).attr("href"),$(this).attr("cache"),$(this).attr("iframe"));
+							else if($(this).attr("href").toLowerCase().indexOf("http://") >= 0)
+								loadURL($(this).attr("href"));
+							else
+							{
+								////////////////////////////////getURL("http://smgroup.ir/parse3/" + $(this).attr("href"),$(this).attr("cache"),$(this).attr("iframe"));
+								getURL("http://smgroup.ir" + $(this).attr("href"),$(this).attr("cache"),$(this).attr("iframe"));
+							}
+						});
+						
 						if(cache == true)
 							window.localStorage.setItem($.md5(URL),text);
 					}
