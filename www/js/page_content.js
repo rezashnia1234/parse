bd288587f396cceb68de7e32135d6414d43c645f
rezashnia1234 <reza_shnia@spam4.me>
2015-01-05 11:21:49 +0330

    60

--- www/js/page_content.js
+++ www/js/page_content.js
@@ -28,7 +28,6 @@ function getURL(URL,cache,iframe) {
 		{
 //alert("we have no internet - we have cache");
 			$('.container').html(article_json);
-			do_rotation = false;
 			$('.container a').on("click", function (e) {
 				e.preventDefault();
 				
@@ -45,31 +44,28 @@ function getURL(URL,cache,iframe) {
 				}
 				FastClick.attach(document.body);	
 			});
-			unblockui();
 		}
 		else
 		{
 //alert("we have no internet - no cache");
 			console.log('SMGROUP ::::::::::::::::::::::::::::::::::::    get_URL / no internet AND no cache');
 			$('.container').html("براي مشاهده اين صفحه نياز به اينترنت داريد");
-			do_rotation = false;
-			unblockui();
 		}
-	} else {
+	}
+	else
+	{
 //alert("we have internet");
 		if(iframe == false)
 		{
 //alert("we have internet - iframe : false");
-			console.log('SMGROUP ::::::::::::::::::::::::::::::::::::    get_URL / Start Download JSON For cache');
-			$.ajax({ type: "GET",   
-					url: URL,
-					async: true,
-					success : function(text)
+			var article_json = window.localStorage.getItem($.md5(URL));
+			var temp_array = window.sessionStorage.getItem('LOAD_URL');
+			if((jQuery.inArray($.md5(URL),temp_array) != -1)	&&	article_json)
+			{
+					if(article_json)
 					{
-						$('.container').html(text);
-						unblockui();
-						do_rotation = false;
-						
+//alert("we have internet - we have recent cache");
+						$('.container').html(article_json);
 						$('.container a').on("click", function (e) {
 							e.preventDefault();
 							
@@ -86,23 +82,71 @@ function getURL(URL,cache,iframe) {
 							}
 							FastClick.attach(document.body);	
 						});
-						
-						if(cache == true)
-						{
-							window.localStorage.setItem($.md5(URL),text);
-							var temp_array = window.sessionStorage.getItem('LOAD_URL');
-							temp_array.push($.md5(URL));
-							window.sessionStorage.setItem('LOAD_URL',temp_array);
-//alert("we have internet - cached" + $.md5(URL));
-						}
 					}
-			});
+			}
+			else
+			{
+					console.log('SMGROUP ::::::::::::::::::::::::::::::::::::    get_URL / Start Download JSON For cache');
+					$.ajax({ type: "GET",   
+							url: URL,
+							async: true,
+							success : function(text)
+							{
+								$('.container').html(text);
+								
+								$('.container a').on("click", function (e) {
+									e.preventDefault();
+									
+									if($(this).attr("href").toLowerCase().indexOf("http://parseh.smcms.ir") >= 0)
+										openURL($(this).attr("href"),$(this).attr("cache"),$(this).attr("iframe"));
+									else if($(this).attr("href").toLowerCase().indexOf("http://parseh.smcms.ir") >= 0)
+										openURL($(this).attr("href"),$(this).attr("cache"),$(this).attr("iframe"));
+									else if($(this).attr("href").toLowerCase().indexOf("http://") >= 0)
+										loadURL($(this).attr("href"));
+									else
+									{
+										////////////////////////////////openURL("http://smgroup.ir/parse3/" + $(this).attr("href"),$(this).attr("cache"),$(this).attr("iframe"));
+										openURL("http://parseh.smcms.ir" + $(this).attr("href"),$(this).attr("cache"),$(this).attr("iframe"));
+									}
+									FastClick.attach(document.body);	
+								});
+								
+								if(cache == true)
+								{
+									window.localStorage.setItem($.md5(URL),text);
+									temp_array = window.sessionStorage.getItem('LOAD_URL');
+									temp_array.push($.md5(URL));
+									window.sessionStorage.setItem('LOAD_URL',temp_array);
+//alert("we have internet - cached" + $.md5(URL));
+								}
+							},
+							error: function(jqXHR, exception) {
+//alert("we have internet - but we have error : " + exception);
+								if (jqXHR.status === 0) {
+									$('.container').html('Not connect.\n Verify Network.');
+								} else if (jqXHR.status == 404) {
+									$('.container').html('Requested page not found. [404]');
+								} else if (jqXHR.status == 500) {
+									$('.container').html('Internal Server Error [500].');
+								} else if (exception === 'parsererror') {
+									$('.container').html('Requested JSON parse failed.');
+								} else if (exception === 'timeout') {
+									$('.container').html('Time out error.');
+								} else if (exception === 'abort') {
+									$('.container').html('Ajax request aborted.');
+								} else {
+									$('.container').html('Uncaught Error.\n' + jqXHR.responseText);
+								}
+							},
+							//error : function (request, status, error) {
+							//	alert(request.responseText);
+							//}
+					});
+			}
 		}
 		else
 		{
 			$('.container').html("<iframe src='" + URL + "'></iframe>");
-			do_rotation = false;
-			unblockui();
 		}
 		
 	}
